f66124e4f5bb477ad832307535867269c3fe8bd6
Daniel Do <dodaniel006@gmail.com>
2025-10-28 23:25:58 -0700

    Initial functionality for main method.

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,10 +1,67 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main
 {
-    public static final int NONE = 0;
+    private static final int NONE = 0;
+    private static final String[] OPTIONS = { "W", "N", "M", "Q" };
 
     public static void main(final String[] args)
     {
-        int end;
+        final StringBuilder sb;
+        boolean runProgram;
+        Scanner userInput;
+        String entryMessage;
+
+        sb = new StringBuilder();
+        runProgram   = true;
+        userInput    = new Scanner(System.in);
+
+        sb.append("Please select an option (case insensitive) from the menu below:\n");
+        sb.append("W: Word Game\n");
+        sb.append("N: Number Game\n");
+        sb.append("M: My Game\n");
+        sb.append("Q: Quit Program\n");
+
+        System.out.println("Welcome to Daniel Do's Term Project.\n");
+        entryMessage = sb.toString();
+
+        while (runProgram)
+        {
+            boolean isValidInput;
+            isValidInput = false;
+            do
+            {
+                System.out.println(entryMessage);
+
+                final String input;
+
+                input = userInput.nextLine().trim().toUpperCase();
+                isValidInput = Arrays.asList(OPTIONS).contains(input);
+
+                if (isValidInput)
+                {
+                    switch (input) {
+                        case "W":
+                            System.out.println("Starting Word Game...\n");
+                            break;
+
+                        case "N":
+                            System.out.println("Starting Number Game...\n");
+                            break;
+
+                        case "M":
+                            System.out.println("Starting My Game...\n");
+                            break;
+
+                        case "Q":
+                            System.out.println("Quitting Program. Goodbye!");
+                            runProgram = false;
+                            break;
+                    }
+                }
+            } while (!isValidInput);
+        }
 
 
     }
