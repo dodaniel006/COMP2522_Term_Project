6c4e50e55f8ad9006960a0f363afa90cc1c08ea9
Daniel Do <dodaniel006@gmail.com>
2025-10-31 14:44:22 -0700

    Add javadocs for World class

--- src/code/World.java
+++ src/code/World.java
@@ -1,16 +1,31 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A class representing a World containing {@link Country} Objects
+ * mapped to their respective continent names.
+ *
+ * @author Daniel Do
+ * @version 1.0
+ */
 final class World
 {
     private final Map<Country, String> countries;
 
+    /**
+     * Constructor for World Class.
+     *
+     * @param countries a HashMap of Country objects to their respective continent names
+     */
     public World(final Map<Country, String> countries)
     {
         verifyCountries(countries);
         this.countries = countries;
     }
 
+    /*
+     * Verifies that the countries map is neither null nor empty and is of type HashMap.
+     */
     private static void verifyCountries(final Map<Country, String> countries)
     {
         final String countriesClass;
@@ -28,6 +43,11 @@ final class World
         }
     }
 
+    /**
+     * Getter for the countries map.
+     *
+     * @return the countries map
+     */
     public Map<Country, String> getCountries()
     {
         return countries;
